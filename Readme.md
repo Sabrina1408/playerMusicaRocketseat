021fc0d5f55971df70fbd03326ddbab633f908be
Sabrina Alves Brito <sabrina.a.brito@gmail.com>
2023-01-19 15:54:54 -0300

    range e timer funcionando

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
-![image](https://user-images.githubusercontent.com/88604193/212443680-77762abc-a088-459b-a14a-462bd1bfe461.png)
+![image](https://user-images.githubusercontent.com/88604193/213534306-1769304a-873a-4808-8278-6481f90482c7.png)
 <h1>👩🏽‍💻 Player de musica - Rocketseat</h1>
-<p>Funcionalidades: Play e pause, voltar música e passar música, próxima musica automática. Feito com HTML, CSS e JavaScript.</p>
+<p>Funcionalidades: Play e pause, voltar e passar música, próxima musica automática. Feito com HTML, CSS e JavaScript. Pensando em fazer integração para para consumir api no futuro.</p>
 <h2>🎯 Links</h2>
 <ul>
   <li>
@@ -10,9 +10,8 @@
     <a href="https://sabrinaalves.tk" target="_blank">Meu portfólio</a>
   </li>
 </ul>
-<h2>👽 Status</h2>
+<h2>👽 Status - Em construção</h2>
 <ul>
-  <li>Em construção.</li>
   <li>Fazendo barra de range podendo mudar o tempo da música.</li>
   <li>Fazendo funcionalidades para os players na horizontal.</li>
 </ul>
@@ -20,8 +19,25 @@
 <ul>
   <li>Manipulação de audio.</li>
   <li>Elementos relacionados a audio.</li>
-  <li>Ended event.</li>
+  <li>Ended, change, load e timeupdate event.</li>
   <li>Range input</li>
+  <li>!isNaN</li>
+</ul>
+<h2>👀 Ideias/Problemas/Fazer</h2>
+<ul>
+  <li>Toda vez que aperta play reinicia.</li>
+  <li>Duração final e posição atual não estão vindo na forma para aparecer na aplicação.</li>
+  <li>Barra de range com css errado.</li>
+  <li>Imagem do player 2 cortando.</li>
+  <li>Não está funcionando no Opera, mas funciona no Edge e Chrome.</li>
+  <li>Back e Next estão com i errado.</li>
+  <li>Script pros outros players.</li>
+  <li>Talvez mutar em outros players, mas deixar o resto das funcionalidades funcionando.</li>
+  <li>Melhorar código, talvez fazer em classe.</li>
+</ul>
+<h2>🎨 Figma</h2>
+<ul>
+  <li><a href="https://www.figma.com/file/zsBLDiODKQrJNDtBvfVkmW/%23boraCodar---Desafio-1-(Copy)?node-id=1%3A61&t=ulDmj9I0YHaZwt84-0" target="_blank">#boraCodar - Desafio 1</a></li>
 </ul>
 <h2>✌️ Referências</h2>
 <ul>
@@ -33,20 +49,5 @@
   <li><a href="https://www.youtube.com/watch?v=vOBlIR8cneg" target="_blank">JS #S35 🌟 How to get video duration length time in seconds Vanilla JavaScript full Crash Course 2021</a></li>
   <li><a href="https://www.youtube.com/watch?v=ClT_hbwF5qU" target="_blank">JavaScript Music Player Tutorial - #9 of 12 (Update Track Duration )</a></li>
 </ul>
-<h2>👀 Problemas/fazer</h2>
-<ul>
-  <li>Toda vez que aperta play reinicia.</li>
-  <li>Duração final e posição atual não estão vindo na forma para aparecer na aplicação.</li>
-  <li>Botar o tempo da música na aplicação.</li>
-  <li>Toda vez que aperta play reinicia.</li>
-  <li>Barra de range ainda não está mudando.</li>
-  <li>Não está funcionando no Opera, mas funciona no Edge e Chrome.</li>
-</ul>
-<h2>🎨 Figma</h2>
-<a href="https://www.figma.com/file/zsBLDiODKQrJNDtBvfVkmW/%23boraCodar---Desafio-1-(Copy)?node-id=1%3A61&t=ulDmj9I0YHaZwt84-0" target="_blank">#boraCodar - Desafio 1</a>
 
-<!-- https://www.youtube.com/watch?v=vqrjFnq3-uo -->
-<!-- https://www.youtube.com/watch?v=oscPp3KghS8 -->
-<!-- https://www.figma.com/file/zsBLDiODKQrJNDtBvfVkmW/%23boraCodar---Desafio-1-(Copy)?node-id=1%3A61&t=ulDmj9I0YHaZwt84-0 -->
-<!-- https://www.w3schools.com/tags/ref_av_dom.asp -->
 <!-- https://www.youtube.com/watch?v=9uGAZ8MFo88 -->
--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,15 +1,12 @@
 const player = {
-    audio: document.createElement("audio"), /* mudar pra create */
+    audio: document.createElement("audio"),
     play: document.querySelector(".play"),
     back: document.querySelector(".back"),
     next: document.querySelector(".next"),
     rangeMusic: document.querySelector(".rangeMusic"),
     currentTime: document.querySelector(".currentTime"),
     totalDuration: document.querySelector(".totalDuration"),
-    /* https://www.youtube.com/watch?v=vqrjFnq3-uo */
-
     playPauseImage: document.querySelector(".playPauseImage"),
-
     h1Player: document.querySelectorAll(".h1Player"),
     h2Player: document.querySelectorAll(".h2Player"),
     imgPlayer: document.querySelectorAll(".imgPlayer"),
@@ -17,21 +14,27 @@ const player = {
         {
             nameSong: "Acorda Devinho",
             artist: "Banda Rocketseat",
-            img: "public/assets/img/purple.png",
+            img: "public/assets/img/acordapedrinho.png",
             song: "public/assets/audio/acorda-pedrinho.mp3"
         },
         {
             nameSong: "Universo de coisas que eu desconheço",
             artist: "Lagum e Anavitória",
-            img: "public/assets/img/unsplash.jpg",
+            img: "public/assets/img/universo.jpg",
             song: "public/assets/audio/universo-de-coisas-que-eu-desconheço.mp3"
+        },
+        {
+            nameSong: "MAMA.CITA (hasta la vista)",
+            artist: "Luísa Sonza, Xamã",
+            img: "public/assets/img/mamacita.jpg",
+            song: "public/assets/audio/mama-cita-hasta-la-vista.mp3"
         }
     ],
     start() {
         let i = 0;
-        let updateTimer;
-        clearInterval(updateTimer);
-        reset();
+        let timerUpdate;
+        resetRange();
+
         function musicInformation() {
             player.h1Player.forEach(element => {
                 element.innerHTML = player.list[i].nameSong;
@@ -42,40 +45,70 @@ const player = {
             player.imgPlayer.forEach(element => {
                 element.src = player.list[i].img;
             });
+            rangeUpdate = setInterval(() => {
+                let position = 0;
+                if(!isNaN(player.audio.duration)) {
+                    position = player.audio.currentTime * (100/player.audio.duration);
+                    player.rangeMusic.value = position;
+                }
+            }, 1000);
+            clearInterval(timerUpdate);
         }
+
         window.addEventListener("load", () => {
             console.log("page is fully loaded");
             musicInformation();
         });
-        window.onload = musicInformation();
-        /* Fazer */
-        function reset() {
+
+        function resetRange() {
             player.currentTime.textContent = "00:00";
             player.totalDuration.textContent = "00:00";
             player.rangeMusic.value = 0;
         }
+
+        player.audio.addEventListener("timeupdate", () => {
+            if(player.audio.duration){
+                let curMins = Math.floor(player.audio.currentTime/60);
+                let curSecs = Math.floor(player.audio.currentTime - curMins*60);
+                let durMins = Math.floor(player.audio.duration/60);
+                let durSecs = Math.floor(player.audio.duration - durMins*60);
+                if(durSecs<10){
+                    durSecs = "0"+durSecs;
+                }
+                if(durMins<10){
+                    durMins = "0"+durMins;
+                }
+                if(curMins<10){
+                    curMins = "0"+curMins;
+                }
+                if(curSecs<10){
+                    curSecs = "0"+curSecs;
+                }
+                player.currentTime.innerHTML = `${curMins}:${curSecs}`;
+                player.totalDuration.innerHTML = `${durMins}:${durSecs}`;
+            } else {
+                /* player.currentTime.innerHTML = "00:00";
+                player.totalDuration.innerHTML = "00:00"; */
+            }
+        });
+
         this.play.addEventListener("click", () => {
             if(this.audio.paused == true) {
+                console.log("play: "+i);
                 this.audio.src = this.list[i].song;
                 this.audio.play();
                 musicInformation();
                 this.playPauseImage.src = "public/assets/img/pause.svg";
-                /* Fazer */
-                updateTimer = setInterval(() => {
-                    console.log(Math.floor(this.audio.currentTime/60));
-                    console.log(Math.floor(this.audio.duration/60));
-                    /* console.log((this.audio.currentTime/60).toFixed(2));
-                    console.log((this.audio.duration/60).toFixed(2)); */
-                    /* console.log((this.audio.currentTime/(this.audio.duration/60)*100).toFixed(2))
-                    player.rangeMusic.style.width = (this.audio.currentTime/60*100).toFixed(2); */
-                }, 1000); // Se apertar mais ta indo mais rápido
-                /* let rangeMusicSeparate = this.audio.duration * (this.rangeMusic.value/100);
-                this.currentTime = rangeMusicSeparate; // Não tá pegando */
+                timerUpdate = setInterval(() => {
+                    presentTime = Math.floor(this.audio.currentTime);
+                    totalDuration = (this.audio.duration/60).toFixed(2)
+                }, 1000);
             } else {
                 this.audio.pause();
                 this.playPauseImage.src = "public/assets/img/play.svg";
             }
         });
+
         this.audio.addEventListener("ended", () => {
             if(i < this.list.length-1){
                 i+=1;
@@ -89,14 +122,13 @@ const player = {
                 musicInformation();
             }
         });
-        /* fazer */
         this.rangeMusic.addEventListener("change", () => {
             let positionRange = this.audio.duration * (player.rangeMusic.value/100);
-            console.log(player.rangeMusic.value);
             this.audio.currentTime = positionRange;
         });
     
         this.back.addEventListener("click", () => {
+            console.log("back: "+i);
             i--;
             if(i<=this.list.length){
                 i=0;
@@ -105,10 +137,12 @@ const player = {
                 this.playPauseImage.src = "public/assets/img/pause.svg";
             }
             musicInformation();
-            this.audio.src = this.list[i].song; /* v */
+            this.audio.src = this.list[i].song;
             this.audio.play();
         });
+
         this.next.addEventListener("click", () => {
+            console.log("next: "+i);
             i++;
             if(i>=this.list.length){
                 i=0;
@@ -117,18 +151,14 @@ const player = {
                 this.playPauseImage.src = "public/assets/img/pause.svg";
             }
             musicInformation();
-            this.audio.src = this.list[i].song; /* v */
+            this.audio.src = this.list[i].song;
             this.audio.play();
         });
     }
 }
 player.start();
 
-// Botar o range
-// Botar o tempo da música
+// back e next estão com i errado
 // Pegar todos os elementos
 // Não está funcionando no Opera, mas funciona no Edge e Chrome
 // Pause vai mais reinicia a musica
-
-//https://www.w3schools.com/tags/ref_av_dom.asp
-//https://www.youtube.com/watch?v=vOBlIR8cneg
